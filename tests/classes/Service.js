fc2d00ab20d0b1691b750efd0c540d753fc79ebc
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-08-15 21:32:26 +0900

    added test coverage for new variable system

--- tests/classes/Service.js
+++ tests/classes/Service.js
@@ -25,7 +25,7 @@ describe('Service', () => {
       expect(serviceInstance.defaults).to.deep.equal({
         stage: 'dev',
         region: 'us-east-1',
-        variableSyntax: "\\${([\\s\\S]+?)}",
+        variableSyntax: '\\${([\\s\\S]+?)}',
       });
       expect(serviceInstance.custom).to.deep.equal({});
       expect(serviceInstance.plugins).to.deep.equal([]);
@@ -98,6 +98,617 @@ describe('Service', () => {
 
       expect(serviceInstance.provider.name).to.be.equal('testProvider');
       expect(serviceInstance.provider.runtime).to.be.equal('nodejs4.3');
+    });
+  });
+
+  describe('#load()', () => {
+    let serviceInstance;
+
+    it('should resolve if no servicePath is found', (done) => {
+      const serverless = new Serverless();
+      const noService = new Service(serverless);
+
+      return noService.load().then(() => done());
+    });
+
+    it('should load from filesystem', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        service: 'new-service',
+        provider: 'aws',
+        defaults: {
+          stage: 'dev',
+          region: 'us-east-1',
+          variableSyntax: '\\${{([\\s\\S]+?)}}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        expect(serviceInstance.service).to.be.equal('new-service');
+        expect(serviceInstance.provider).to.deep.equal({ name: 'aws' });
+        expect(serviceInstance.defaults.variableSyntax).to.equal('\\${{([\\s\\S]+?)}}');
+        expect(serviceInstance.plugins).to.deep.equal(['testPlugin']);
+        expect(serviceInstance.resources.aws).to.deep.equal({ resourcesProp: 'value' });
+        expect(serviceInstance.resources.azure).to.deep.equal({});
+        expect(serviceInstance.resources.google).to.deep.equal({});
+        expect(serviceInstance.package.include.length).to.equal(1);
+        expect(serviceInstance.package.include[0]).to.equal('include-me.js');
+        expect(serviceInstance.package.exclude.length).to.equal(1);
+        expect(serviceInstance.package.exclude[0]).to.equal('exclude-me.js');
+        expect(serviceInstance.package.artifact).to.equal('some/path/foo.zip');
+      });
+    });
+
+    it('should support Serverless file with a .yaml extension', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYaml = {
+        service: 'my-service',
+        provider: 'aws',
+        functions: {
+          functionA: {
+            name: 'customFunctionName',
+          },
+        },
+      };
+
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yaml'),
+        YAML.dump(serverlessYaml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        const expectedFunc = {
+          functionA: {
+            name: 'customFunctionName',
+            events: [],
+          },
+        };
+        expect(serviceInstance.service).to.be.equal('my-service');
+        expect(serviceInstance.provider).to.deep.equal({ name: 'aws' });
+        expect(serviceInstance.functions).to.deep.equal(expectedFunc);
+      });
+    });
+
+    it('should support Serverless file with a .yml extension', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        service: 'my-service',
+        provider: 'aws',
+        functions: {
+          functionA: {},
+        },
+      };
+
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load({ stage: 'dev' }).then(() => {
+        const expectedFunc = {
+          functionA: {
+            name: 'my-service-dev-functionA',
+            events: [],
+          },
+        };
+        expect(serviceInstance.service).to.be.equal('my-service');
+        expect(serviceInstance.provider).to.deep.equal({ name: 'aws' });
+        expect(serviceInstance.functions).to.deep.equal(expectedFunc);
+      });
+    });
+
+    it('should throw error if service property is missing', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        provider: 'aws',
+        functions: {},
+      };
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        // if we reach this, then no error was thrown as expected
+        // so make assertion fail intentionally to let us know something is wrong
+        expect(1).to.equal(2);
+      }).catch(e => {
+        expect(e.name).to.be.equal('ServerlessError');
+      });
+    });
+
+    it('should throw error if provider property is missing', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        service: 'service-name',
+        functions: {},
+      };
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        // if we reach this, then no error was thrown as expected
+        // so make assertion fail intentionally to let us know something is wrong
+        expect(1).to.equal(2);
+      }).catch(e => {
+        expect(e.name).to.be.equal('ServerlessError');
+      });
+    });
+
+    it('should throw error if functions property is missing', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        service: 'service-name',
+        provider: 'aws',
+      };
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        // if we reach this, then no error was thrown as expected
+        // so make assertion fail intentionally to let us know something is wrong
+        expect(1).to.equal(2);
+      }).catch(e => {
+        expect(e.name).to.be.equal('ServerlessError');
+      });
+    });
+
+    it('should throw error if provider property is invalid', () => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYml = {
+        service: 'service-name',
+        provider: 'invalid',
+        functions: {},
+      };
+
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yml'),
+        YAML.dump(serverlessYml));
+
+      const serverless = new Serverless({ servicePath: tmpDirPath });
+      serviceInstance = new Service(serverless);
+
+      return serviceInstance.load().then(() => {
+        // if we reach this, then no error was thrown as expected
+        // so make assertion fail intentionally to let us know something is wrong
+        expect(1).to.equal(2);
+      }).catch(e => {
+        expect(e.name).to.be.equal('ServerlessError');
+      });
+    });
+  });
+
+  describe('#populate()', () => {
+    const serverless = new Serverless();
+    it('should populate from environment variables', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customProp: 'value',
+          envVarRef: '${env.TEST_VAR}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+
+      const serviceInstance = new Service(serverless, data);
+      process.env.TEST_VAR = 'someValue';
+      const populatedService = serviceInstance.populate();
+      expect(populatedService.custom.envVarRef).to.equal('someValue');
+      delete process.env.TEST_VAR;
+    });
+
+    it('should populate from options', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customProp: 'value',
+          optVarRef: '${opt.stage}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const options = {
+        stage: 'prod',
+      };
+
+      const serviceInstance = new Service(serverless, data);
+      const populatedService = serviceInstance.populate(options);
+      expect(populatedService.custom.optVarRef).to.equal('prod');
+    });
+
+    it('should populate entire options object', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customProp: 'value',
+          optVarRef: '${opt}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const options = {
+        stage: 'prod',
+        region: 'us-east-1',
+      };
+
+      const serviceInstance = new Service(serverless, data);
+      const populatedService = serviceInstance.populate(options);
+      expect(populatedService.custom.optVarRef).to.deep.equal(options);
+    });
+
+    it('should populate from deep string properties in self service', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          selfVarRef: 'this is a string: ${self.custom.customObj.prop2.subProp1}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      const populatedService = serviceInstance.populate();
+      expect(populatedService.custom.selfVarRef)
+        .to.equal('this is a string: subProp1');
+    });
+
+    it('should populate from deep any type properties in self service', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          selfVarRef: '${self.custom.customObj.prop1}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      const populatedService = serviceInstance.populate();
+      expect(populatedService.custom.selfVarRef)
+        .to.equal(data.custom.customObj.prop1);
+      expect(typeof populatedService.custom.selfVarRef)
+        .to.equal('number');
+    });
+
+    it('should throw error when populating non string as substring from self service', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          selfVarRef: 'this is a string: ${self.custom.customObj.prop1}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate())
+        .to.throw(Error);
+    });
+
+    it('should throw error when populating from self service with invalid property', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          selfVarRef: '${self.custom.invalidCustomObj.prop3}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate())
+        .to.throw(Error);
+    });
+
+    it('should throw error when populating env vars strings as objects', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          envVarRef: '${env.var.subProp}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate())
+        .to.throw(Error);
+    });
+
+    it('should throw error when populating option strings as objects', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          optVarRef: '${opt.stage.subProp}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const options = {
+        stage: 'prod',
+        region: 'us-east-1',
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate(options))
+        .to.throw(Error);
+    });
+
+    it('should throw error when populating option obj as sub string', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          optVarRef: 'this is a string: ${opt}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const options = {
+        stage: 'prod',
+        region: 'us-east-1',
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate(options))
+        .to.throw(Error);
+    });
+
+    it('should throw error when referencing invalid source', () => {
+      const data = {
+        service: 'testService',
+        provider: 'testProvider',
+        custom: {
+          customObj: {
+            prop1: 1,
+            prop2: {
+              subProp1: 'subProp1',
+            },
+          },
+          varRef: '${src.var}',
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {},
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value',
+          },
+          azure: {},
+          google: {},
+        },
+        package: {
+          include: ['include-me.js'],
+          exclude: ['exclude-me.js'],
+          artifact: 'some/path/foo.zip',
+        },
+      };
+      const serviceInstance = new Service(serverless, data);
+      expect(() => serviceInstance.populate())
+        .to.throw(Error);
     });
   });
 
